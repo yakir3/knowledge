5027d41be24189afa246d661c788d3878e5b1907
yakir <yakir@acclub.io>
2024-05-24 11:38:32 +0800

    add Development scripts dir

--- Development/Bash/README.md
+++ Development/Bash/README.md
@@ -208,64 +208,6 @@ result="${variable%%/*}"   # longest match from end, get: path
 
 ```
 
-### template.sh
-```shell
-#!/bin/bash
-
-# help info
-### template.sh -— call xxx command to do something
-###
-### Usage:
-###   ./template.sh <arg1> <arg2> [option1]
-###
-### Options:
-###   <arg1>       arg1: xxx
-###   <arg2>       arg2: xxx
-###   [option1]    option1: xxx
-###
-### Examples:
-###   # Show this message
-###   ./template.sh -h
-###   # Do event 1
-###   ./template.sh aaa bbb
-###   # Do event 2
-###   ./template.sh xxx yyy
-###   # Do event 3
-###   ./template.sh arg1 arg2 option1
-function help_info() {
-  sed -rn 's/^### ?//;T;p;' "$0"
-  exit 1
-}
-
-# logging
-function log_info() {
-  local message="$@"
-  echo "[INFO] $message"
-}
-function log_warning() {
-  local message="$@"
-  echo "[WARNING] $message" >&2
-}
-function die_exit() {
-  local message="$@"
-  echo "[ERROR] $message" 1>&2
-  exit 111
-}
-
-
-# global environment
-key1=value1
-timeout=120s
-
-
-# main function
-function main() {
-    # help info
-    [[ $# -lt 2 ]] || [[ $1 == "-h" ]] && help_info
-}
-
-main "$@"
-```
 
 
 >Reference:
--- Development/Golang/scripts/check-realip.go
+++ Development/Golang/scripts/check-realip.go
@@ -6,13 +6,16 @@
 package main
 
 import (
-    "bytes"
+    //"bytes"
+    "crypto/tls"
     "encoding/json"
     "log"
     "io"
     "net/http"
+    "net/smtp"
     "os"
-    //"reflect"
+    "reflect"
+    "strings"
 )
 
 type RealipData struct {
@@ -32,9 +35,9 @@ func init() {
     //if err != nil {
     //    panic("Error creating or open log file")
     //}
-    dlogger = log.New(os.Stdout, "Log: ", log.Ldate|log.Ltime|log.Lshortfile)
     //dlogger = log.New(logFile, "Log: ", log.Ldate|log.Ltime|log.Lshortfile)
     //dlogger = log.New(io.MultiWriter(os.Stdout, logFile), "Log: ", log.Ldate|log.Ltime|log.Lshortfile)
+    dlogger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 //func getCloudflare() {
@@ -42,14 +45,11 @@ func init() {
 //}
 
 func getCloudfront() {
-    //defer logFile.Close()
-
     // 定义 cloudfront 接口地址以及发起 http 请求
     cfURL := "https://d7uri8nf7uskq.cloudfront.net/tools/list-cloudfront-ips"
     resp, err := http.Get(cfURL)
     if err != nil {
         dlogger.Fatal("Error request to cloudfront api:", err)
-        return
     }
     defer resp.Body.Close()
 
@@ -57,12 +57,10 @@ func getCloudfront() {
     body, err := io.ReadAll(resp.Body)
     if err != nil {
         dlogger.Fatal("Error reading response:", err)
-        return
     }
     var respData = make(map[string][]string)
     if err := json.Unmarshal(body, &respData); err != nil {
         dlogger.Fatal("Error decoding JSON:", err)
-        return
     }
     var newSlice = make([]string, 0)
     newSlice = append(append(newSlice, respData["CLOUDFRONT_GLOBAL_IP_LIST"]...), respData["CLOUDFRONT_REGIONAL_EDGE_IP_LIST"]...)
@@ -70,23 +68,20 @@ func getCloudfront() {
     // 已读写方式打开 realip.json 文件
     file, err := os.OpenFile("./realip.json", os.O_CREATE|os.O_RDWR, 0644)
     if err != nil {
-        dlogger.Println("Error creating or open file:", err)
-        return
+        dlogger.Fatal("Error creating or open file:", err)
     }
     defer file.Close()
     oldContent := make([]byte, 10240)
     n, err := file.Read(oldContent)
     if err != nil {
-        dlogger.Println("Error reading file:", err)
-        return
+        dlogger.Fatal("Error reading file:", err)
     }
     oldContentStr := string(oldContent[:n])
 
     // 初始化 RealipData 结构体, 将文件内容序列化为结构体数据类型, 获取 cloudfront 数据切片 oldSlice
     var realipData RealipData
     if err := json.Unmarshal([]byte(oldContentStr), &realipData); err != nil {
-        dlogger.Println("Error decoding JSON:", err)
-        return
+        dlogger.Fatal("Error decoding JSON:", err)
     }
     //dlogger.Println(realipData)
     oldSlice := realipData.Cloudfront
@@ -94,11 +89,15 @@ func getCloudfront() {
     // 对比 oldSlice 与 newSlice 切片是否有差值. 有差值: 发出通知邮件, 邮件发送成功更新接口内容到文件中
     var diff = diffSlice(oldSlice, newSlice)
     if len(diff) == 0 {
-        dlogger.Println("AWS Cloudfront RealIP 无更新.")
+        dlogger.Println("AWS Cloudfront Realip No Update.")
     } else {
-        dlogger.Println(len(diff))
         // 发送通知邮件
-        // sendEmail()
+        sd := "Notice<notice@example.com>"
+        rcv := "Yakir<yakir@example.com>"
+        sbj := "AWS Cloudfront Realip Update Notification"
+        msg := strings.Join(diff, "\n")
+        //sendEmail(sd, rcv, sbj, msg)
+        dlogger.Println(sd, rcv, sbj, reflect.TypeOf(msg))
 
         // 新切片数据 newSlice 写回 RealipData 结构体
         realipData.Cloudfront = newSlice
@@ -106,17 +105,17 @@ func getCloudfront() {
         if err != nil {
             dlogger.Println("Error encoding JSON:", err)
         }
+        dlogger.Println(reflect.TypeOf(realipDataByte))
 
         // 结构体数据 realipData 回写文件 real.json
-        // option1: io
-        //if _, err := io.Copy(file, bytes.NewReader(realipDataByte)); err != nil {
-        //    dlogger.Println("Error writing file:", err)
+        // option1: os.File WriteAt
+        //if _, err = file.WriteAt(realipDataByte, 0); err != nil {
+        //    dlogger.Println("Error writing data:", err)
         //}
-        // option2: file.WriteAt
-        //_, err = file.WriteAt(realipDataByte, 0)
-        //if err != nil {
-        //    dlogger.Println("Error writing file:", err)
-        //    return
+        // option2: io.WriterAt WriteAt
+        //writer := io.WriterAt(file)
+        //if _, err = writer.WriteAt(realipDataByte, 0); err != nil {
+        //    dlogger.Println("Error writing data:", err)
         //}
     }
 
@@ -143,7 +142,79 @@ func diffSlice(oslice, nslice []string) []string {
     return diff
 }
 
+// 发送邮件函数
+func sendEmail(sd, rcv, sbj, msg string) {
+    // Set up authentication information.
+    from := sd
+    //password := ""
+    smtpHost := "172.22.1.21"
+    smtpPort := "25"
+    //auth := smtp.PlainAuth("", from, password, smtpHost)
+
+    // Connect to the server, authenticate, set the sender and recipient,
+    // and send the email all in one step.
+    to := []string{rcv}
+    subject := sbj
+    message := []byte("To: " + to[0] + "\r\n" +
+                  "Subject: " + subject + "\r\n" +
+                  "\r\n" +
+                  msg + "\r\n")
+
+    // SMTP Client
+    conn, err := smtp.Dial(smtpHost + ":" + smtpPort)
+    if err != nil {
+        dlogger.Println("Error connecting to SMTP server:", err)
+        return
+    }
+    defer conn.Close()
+    // STARTTLS: Disable TLS verification
+    tlsConfig := &tls.Config{
+        InsecureSkipVerify: true,
+    }
+    if ok, _ := conn.Extension("STARTTLS"); ok {
+        if err := conn.StartTLS(tlsConfig); err != nil {
+            dlogger.Println("Error starting TLS:", err)
+            return
+        }
+    }
+    //// Auth
+    //if err := conn.Auth(auth); err != nil {
+    //    dlogger.Println("Error authenticating:", err)
+    //    return
+    //}
+
+    // Send mail
+    if err := conn.Mail(from); err != nil {
+        dlogger.Println("Error setting from address:", err)
+        return
+    }
+    if err := conn.Rcpt(to[0]); err != nil {
+        dlogger.Println("Error adding recipient:", err)
+        return
+    }
+    w, err := conn.Data()
+    if err != nil {
+        dlogger.Println("Error starting data:", err)
+        return
+    }
+    _, err = w.Write(message)
+    if err != nil {
+        dlogger.Println("Error writing message:", err)
+        return
+    }
+    err = w.Close()
+    if err != nil {
+        dlogger.Println("Error closing data:", err)
+        return
+    }
+    dlogger.Println("Email sent successfully!")
+
+    return
+}
+
 func main() {
+    //defer logFile.Close()
+
     //getCloudflare()
     getCloudfront()
 }
